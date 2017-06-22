e1d0830399b1eab51095217895ae4d9bf5f21932
Max Fierro <elmasse@gmail.com>
2017-06-21 22:44:55 -0300

    removed unused code

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,27 +1,8 @@
 
 import React, { Component } from 'react'
 
-// import remark from 'remark'
-// import reactRenderer from 'remark-react'
-// import select from 'unist-util-select'
-
 import loadEntries from '../load-entries'
-// import Excerpt from './excerpt'
 
-// const excerpt = (selector = 'paragraph:first-child') => (tree) => {
-//   tree.children = select(tree, selector)
-//   return tree
-// }
-
-// const toReact = ({ data, content }, idx) => {
-//   return {
-//     data,
-//     content: remark()
-//       .use(excerpt)
-//       .use(reactRenderer, { prefix: `key-${idx}-` })
-//       .processSync(content).contents
-//   }
-// }
 
 export const inCategory = (category) => (m) => {
   return category ? (m.data.category == category) : true
