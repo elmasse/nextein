72b6fcfae05b84cbb9ad2aeb4b05ad93a0799f9e
Max Fierro <elmasse@gmail.com>
2017-07-22 13:21:28 -0300

    unit test for load-entrie

---
+++ test/unit/load-entries.test.js
@@ -0,0 +1,69 @@
+jest.mock('glob')
+jest.mock('fs')
+jest.mock('frontmatter')
+
+import loadEntries from '../../src/load-entries'
+import glob from 'glob'
+import { readFileSync, statSync } from 'fs'
+import fm from 'frontmatter'
+
+describe('loadEntries', () => {
+  test('exports loadEntries fn as default', () => {
+    expect(loadEntries).toBeDefined()
+  })
+})
+
+describe('loadEntries called from Server', () => {
+
+  test('retrieves an array of posts', async () => {
+    const expectedPage = 'post'
+    const expectedCategory = 'category'
+    const expectedEntry = './posts/test.md'
+    const expectedContent = 'text'
+    const expectedUrl = `/${expectedCategory}/test`
+    const expectedDate = new Date()
+    const expectedFileContent = `
+        ---
+        page: ${expectedPage}
+        category: ${expectedCategory}
+        ---
+        ${expectedContent}
+        `
+
+    glob.sync.mockReturnValueOnce([
+      './posts/test.md'
+    ])
+
+    readFileSync.mockReturnValueOnce(expectedFileContent)
+
+    fm.mockReturnValueOnce({
+      data: {
+        page: expectedPage,
+        category: expectedCategory
+      },
+      content: expectedContent
+    })
+
+    statSync.mockReturnValueOnce({
+      birthtime: expectedDate
+    })
+
+    const actual = await loadEntries()
+
+    expect(actual).toEqual(expect.any(Array))
+    expect(actual).toEqual(expect.arrayContaining([
+      {
+        data: {
+          page: expectedPage,
+          category: expectedCategory,
+          date: expectedDate.toJSON(),
+          url: expectedUrl,
+          _entry: expectedEntry
+        },
+        content: expectedContent
+      }
+    ]))
+
+  })
+})
+
