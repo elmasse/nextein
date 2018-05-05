14feb29a9946d4b088e7ba1f279f98b411a81413
Max Fierro <elmasse@gmail.com>
2018-05-04 21:06:42 -0300

    remove raw from rest props in link

--- src/components/link.js
+++ src/components/link.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 
 const NexteinLink = (props) => {
-  const { data, children, content, ...rest } = props
+  const { data, children, content, raw, ...rest } = props // content & raw are not used but required to remove them from rest
   let { href, as } = rest
   if (data) {
     const { page = 'post', _entry, url } = data
