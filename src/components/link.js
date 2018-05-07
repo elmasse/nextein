7ea46d6af0e246e3a829247661759a73e924b55a
Max Fierro <elmasse@gmail.com>
2018-05-06 23:10:04 -0300

    make link into a class component

--- src/components/link.js
+++ src/components/link.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Link from 'next/link'
 
-const NexteinLink = (props) => {
-  const { data, children, content, raw, ...rest } = props // content & raw are not used but required to remove them from rest
-  let { href, as } = rest
-  if (data) {
-    const { page = 'post', _entry, url } = data
-    href = { pathname: `/${page}`, query: { _entry } }
-    as = url
+class NexteinLink extends Comment {
+  render() {
+    const { data, children, content, raw, ...rest } = this.props // content & raw are not used but required to remove them from rest
+    let { href, as } = rest
+    if (data) {
+      const { page = 'post', _entry, url } = data
+      href = { pathname: `/${page}`, query: { _entry } }
+      as = url
+    }
+  
+    return <Link {...{...rest, href, as}} >{children}</Link>
   }
-
-  return <Link {...{...rest, href, as}} >{children}</Link>
 }
 
 export default NexteinLink
