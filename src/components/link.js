d567d19bd1563684c7606c8f11470c4c0e44a11f
Max Fierro <elmasse@gmail.com>
2018-05-13 01:28:06 -0300

    Link implementation to pass post url as href. Closes #131

--- src/components/link.js
+++ src/components/link.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
 import Link from 'next/link'
 
+import loadEntries from '../entries/load'
+import { entriesMapReducer } from './utils'
+
 class NexteinLink extends Component {
+  state = {
+    href: this.props.href,
+    as: this.props.as
+  }
+
+  componentWillReceiveProps ({ href, as }) {
+    if (this.state.href !== href) {
+      this.setState({ href, as })
+    }
+  }
+
+  async componentDidMount () {
+    const all = await loadEntries()
+    const map = all.reduce(entriesMapReducer, {})
+    const { href } = this.state
+    if (href) {
+      const entry = map[href]
+      if (entry) {
+        this.setState({
+          href: { pathname: entry.pathname, query: entry.query },
+          as: href
+        })
+      }
+    }
+  }
+
   render () {
+    let { href, as } = this.state
     const { data, children, content, raw, ...rest } = this.props // content & raw are not used but required to remove them from rest
-    let { href, as } = rest
+
     if (data) {
       const { page = 'post', _entry, url } = data
       href = { pathname: `/${page}`, query: { _entry } }
