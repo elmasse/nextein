9e43e56b9221b5597e080aead37af1cc242a0009
Max Fierro <elmasse@gmail.com>
2018-05-13 01:23:04 -0300

    entriesReducer

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,4 +1,13 @@
 
-export function getDisplayName (Component) {
+export const getDisplayName = (Component) => {
   return Component.displayName || Component.name || 'Unknown'
+}
+
+export const entriesMapReducer = (prev, { data }) => {
+  const { url, page, _entry } = data
+  const query = _entry ? { _entry } : undefined
+  return page ? {
+    ...prev,
+    [url]: { pathname: `/${page}`, query }
+  } : prev
 }
