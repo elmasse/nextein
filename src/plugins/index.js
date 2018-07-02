161931a2bb968a1bdc9549aa408f0ebae50e2600
Max Fierro <elmasse@gmail.com>
2018-07-02 19:17:47 -0300

    Let plugins to be local modules.

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,17 +1,31 @@
 import config from 'next/config'
+import { resolve } from 'path'
 
 const INTERNALS = {
-  'nextein-plugin-markdown': './markdown'
+  'nextein-plugin-markdown': resolve(__dirname, 'markdown')
 }
+
+const isLocal = (name) => name.startsWith('./')
+
+const resolvePlugin = (name) => {
+  return INTERNALS[name] || (isLocal(name) ? resolve(process.cwd(), name) : name)
+}
+
 export default () => {
   const { serverRuntimeConfig: { nexteinPlugins } } = config()
   const sources = []
   const transforms = []
   for (const plugin of nexteinPlugins) {
     const [name, options] = plugin
-    const { source, transform } = require(INTERNALS[name] || name)
-    source && sources.push((...args) => source(options, ...args))
-    transform && transforms.push((...args) => transform(options, ...args))
+    const { source, transform } = require(resolvePlugin(name))
+    if (source) {
+      const fn = (...args) => source(options, ...args)
+      fn.__name = name
+      sources.push(fn)
+    }
+    if (transform) {
+      transforms.push((...args) => transform(options, ...args))
+    }
   }
 
   return {
