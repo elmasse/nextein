1aea1b73f8416a66360276fb6c12bf5c8ba1bcfd
Max Fierro <elmasse@gmail.com>
2018-07-03 00:08:42 -0300

    fix default params for watchers

--- src/plugins/markdown/index.js
+++ src/plugins/markdown/index.js
@@ -7,7 +7,7 @@ import pathToRegEx from 'path-to-regexp'
 
 import parser from './parser'
 
-export const watcher = ({ entriesDir = ['posts'] }) => {
+export const watcher = ({ entriesDir = ['posts'] } = {}) => {
   return entriesDir
 }
 
