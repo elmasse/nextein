9454321c69b2606caf3345ce03bb2dc38b90a7d1
Max Fierro <elmasse@gmail.com>
2020-07-17 15:52:13 -0300

    Move to one binary file.

--- taskfile.js
+++ taskfile.js
@@ -4,7 +4,7 @@ export async function compile(task, opts) {
 }
 
 export async function bin(task, opts) {
-   await task.source(opts.src || 'bin/**/*').babel().target('dist/bin', {mode: 0o755})
+  await task.source(opts.src || 'bin/**/*').babel().target('dist/bin', {mode: 0o755})
 }
 
 export async function build(task) {
