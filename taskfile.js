a5f170b5717b7bc92a5a54a3c2270792a6e72140
Max Fierro <elmasse@gmail.com>
2021-11-04 12:25:30 -0300

    improve build.

--- taskfile.js
+++ taskfile.js
@@ -3,10 +3,6 @@ export async function compile (task, opts) {
   await task.source(opts.src || 'src/**/*.js').babel().target('dist/')
 }
 
-// export async function bin (task, opts) {
-//   await task.source(opts.src || 'bin/**/*').babel().target('dist/bin', { mode: 0o755 })
-// }
-
 export async function build (task) {
   await task.serial(['lint', 'compile'])
 }
