fdad2f04c7b22f0bb18e453d351ca0b136a3f1c5
Max Fierro <elmasse@gmail.com>
2021-11-05 08:46:52 -0300

    Make filters and sorters work on posts and metadata

--- src/entries/filters.js
+++ src/entries/filters.js
@@ -1,6 +1,6 @@
 
 export const inCategory = (category, { includeSubCategories = false } = {}) => post => {
-  const { data } = post
+  const { data = post } = post // if there is no post.data, we assume we are working with data object
   const { category: postCategory = '' } = data
   const shouldIncludeSub = includeSubCategories || category.endsWith('/*')
   const result = shouldIncludeSub ? postCategory.startsWith(category.replace('/*', '')) : postCategory === category
