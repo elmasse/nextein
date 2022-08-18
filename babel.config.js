d66bdfdc5d2690c5986d287a0516d1323642fb11
Max Fierro <elmasse@gmail.com>
2022-08-18 08:24:22 -0300

    chore: lint babel config

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  "presets": [
-    "@babel/react",
-    "@babel/env"
+  presets: [
+    '@babel/react',
+    '@babel/env'
   ],
-  "plugins": [
-    "@babel/plugin-transform-runtime",
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-proposal-object-rest-spread"
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread'
   ],
-  "sourceMaps": true,
-  "env": {
-    "test": {
-      "plugins": ["@babel/plugin-transform-modules-commonjs"]
+  sourceMaps: true,
+  env: {
+    test: {
+      plugins: ['@babel/plugin-transform-modules-commonjs']
     }
   }
 }
